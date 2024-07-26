63f9335a1c43d7a1d83f1817e3aac21bb0451c28
Khushi gupta <khushig2603@gmail.com>
2024-07-26 15:32:01 +0530

    dashboard

--- dashboard.py
+++ dashboard.py
@@ -12,20 +12,15 @@ options = {
 
 initialize(**options)
 
+# Function to create dashboard
 def create_dashboard(file_path):
-    try:
-        with open(file_path, 'r') as file:
-            dashboard_data = json.load(file)
-        print(f'Dashboard data for {file_path}:', json.dumps(dashboard_data, indent=2))
-        response = api.Dashboard.create(**dashboard_data)
-        if 'errors' in response:
-            print(f'Error creating dashboard for {file_path}:', response['errors'])
-        else:
-            print(f'Dashboard created for {file_path}:', response)
-    except Exception as e:
-        print(f'Exception occurred while processing {file_path}:', str(e))
+    with open(file_path, 'r') as file:
+        dashboard_data = json.load(file)
+    response = api.Dashboard.create(**dashboard_data)
+    print(f"Dashboard created for {file_path}:", response)
 
 # Find and process all dashboard.json files
-for file_path in glob.glob('client2/dashboard.json'):
+dashboard_files = glob.glob('client*/dashboard.json')
+for file_path in dashboard_files:
     print(f'Processing {file_path}')
     create_dashboard(file_path)
